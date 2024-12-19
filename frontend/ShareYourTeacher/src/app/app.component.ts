900b2ac9dd3c37057eefceff0583dfac2c6c2033
dark-doraemon <tuandinhbi75@gmail.com>
2024-12-19 12:57:29 +0700

    add color picker component

--- frontend/ShareYourTeacher/src/app/app.component.ts
+++ frontend/ShareYourTeacher/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PaintComponent } from "./components/paint/paint.component";
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, PaintComponent,ColorPickerModule,FormsModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ShareYourTeacher';
+    title = 'ShareYourTeacher';
 }
---
+++ frontend/ShareYourTeacher/src/app/components/paint/paint.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
+@Component({
+    selector: 'app-paint',
+    imports: [ColorPickerModule,FormsModule],
+    templateUrl: './paint.component.html',
+    styleUrl: './paint.component.css'
+})
+export class PaintComponent {
+    color: string = '#FF0000'; // Giá trị mặc định
+    
+    
+}
