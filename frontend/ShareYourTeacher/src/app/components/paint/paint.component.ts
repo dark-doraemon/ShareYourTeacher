59af72ea12d4d74d303ac9cb817fb9d7192f3de9
dark-doraemon <tuandinhbi75@gmail.com>
2024-12-21 19:33:51 +0700

    add tools for paint component

--- frontend/ShareYourTeacher/src/app/components/paint/paint.component.ts
+++ frontend/ShareYourTeacher/src/app/components/paint/paint.component.ts
@@ -3,12 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { ColorPickerModule } from 'ngx-color-picker';
 @Component({
     selector: 'app-paint',
-    imports: [ColorPickerModule,FormsModule],
+    imports: [ColorPickerModule, FormsModule],
     templateUrl: './paint.component.html',
     styleUrl: './paint.component.css'
 })
 export class PaintComponent {
     color: string = '#FF0000'; // Giá trị mặc định
-    
+    penSize: number = 5;
+
     
 }
