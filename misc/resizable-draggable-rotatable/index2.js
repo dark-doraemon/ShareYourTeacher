d170a08f3eb90fbdb3ef78442941c957c61c8a5f
dark-doraemon <tuandinhbi75@gmail.com>
2024-12-26 22:34:47 +0700

    index2.js nearly finish

--- misc/resizable-draggable-rotatable/index2.js
+++ misc/resizable-draggable-rotatable/index2.js
@@ -1,54 +1,128 @@
 const box = document.querySelector('.box');
+const textarea = box.querySelector('textarea');
+const rotateLine = box.querySelector('.rotate-line');
 let isDragging = false;
-let isResizing= false
+let isResizing = false;
 let isRotating = false;
+let currentResizer;
 let startX, startY, startWidth, startHeight, startLeft, startTop;
-let minSize = {
-    w: 170,
-    h: 70
+let currentRotation = 0;
+
+function checkTextareaContent() {
+    if (textarea.value.trim() === '') {
+        if(box.style.border === 'none' || box.style.border === '')
+        {
+            textarea.style.border = '1px solid #ccc';
+        }
+    }
+    else {
+        textarea.style.border = 'none';
+    }
 }
 
+// Kiểm tra mỗi khi người dùng thay đổi nội dung trong textarea
+textarea.addEventListener('input', checkTextareaContent);
 
-box.addEventListener('mousedown', (event) => {
-    if (event.target == box || event.target.tagName == 'TEXTAREA') {
+checkTextareaContent();
+// Di chuyển box
+box.addEventListener('mousedown', function (e) {
+    if (e.target === box || e.target.tagName === 'TEXTAREA') {
+            
+        //xuất hiện border và dot
+        textarea.style.border = 'none'
+        resizers.forEach(resizer => {
+            resizer.style.display = 'inline'
+        })
+        box.style.border = "2px solid #333"
+        rotateLine.style.display= 'inline';
+        rotateHandle.style.display= 'inline';
         isDragging = true;
-        //clientX là vị trí của con trỏ trên màn hình tức là khoảng cách từ mép trái của màn hình tới con trỏ theo trục X
-        //clientY là vị trí của con trỏ trên màn hình tức là khoảng cách từ mép trên của màn hình tới con trỏ theo trục Y
-        // box.offsetLeft: là khoảng cách từ mép trái của box đến mép trái của trang web
-        //         Giả sử:
-        // - Box có offsetLeft = 100px (cách mép trái trang 100px)
-        // - Người dùng click tại clientX = 150px
-        // - Thì startX = 150 - 100 = 50px
-        // -> Nghĩa là người dùng click cách mép trái box 50px
-        //tương tự cho trục 
-        startX = event.clientX - box.offsetLeft;
-        startY = event.clientY - box.offsetTop;
-        // Mục đích:
-        // Khi di chuyển box, ta muốn box di chuyển mà vẫn giữ nguyên khoảng cách từ điểm click đến các mép của box
+        startX = e.clientX - box.offsetLeft;
+        startY = e.clientY - box.offsetTop;
+    }
+
+});
+
+document.addEventListener('mousedown', (event) => {
+    if (event.target === box || event.target.tagName === "TEXTAREA") {
+
+    }
+    else {
+        resizers.forEach(resizer => {
+            resizer.style.display = 'none'
+        });
+        box.style.border = 'none'
+        rotateLine.style.display= 'none';
+        rotateHandle.style.display= 'none';
+        checkTextareaContent();
     }
 });
-//lưu ý left và offsetLeft
-//left là khoảng cách từ mép trái của phần từ đến mép trái của phần tử "cha" (có thể gán)
-//offsetLeft là khoảng cách từ mép trái của phần từ đến mép trái của "màn hình"(read-only)
-document.addEventListener('mousemove', (event) => {
+
+// Xoay box
+const rotateHandle = document.querySelector('.rotate-handle');
+rotateHandle.addEventListener('mousedown', function (e) {
+    isRotating = true;
+    const boxRect = box.getBoundingClientRect();
+    //lưu khoảng cách từ mép trái và mép trên của màn hình tới tâm của box
+    const boxCenter = {
+        x: boxRect.left + boxRect.width / 2,
+        y: boxRect.top + boxRect.height / 2
+    };
+    const angle = Math.atan2(e.clientY - boxCenter.y, e.clientX - boxCenter.x);
+    startRotation = angle - currentRotation;
+    e.stopPropagation();
+});
+
+// Điều chỉnh kích thước box
+const resizers = document.querySelectorAll('.dot');
+resizers.forEach(resizer => {
+    resizer.addEventListener('mousedown', function (e) {
+        isResizing = true;
+        currentResizer = e.target;
+        startX = e.clientX;
+        startY = e.clientY;
+        // startWidth = parseFloat(getComputedStyle(box).width);
+        // startHeight = parseFloat(getComputedStyle(box).height);
+        startWidth = box.getBoundingClientRect().width;
+        startHeight = box.getBoundingClientRect().height;
+        startLeft = box.getBoundingClientRect().left;
+        startTop = box.getBoundingClientRect().top;
+        e.stopPropagation();
+    });
+});
+
+document.addEventListener('mousemove', function (e) {
     if (isDragging) {
-        event.preventDefault();
-        box.style.left = (event.clientX - startX) + 'px';
-        box.style.top = (event.clientY - startY) + 'px';
+        e.preventDefault();
+        box.style.left = (e.clientX - startX) + 'px';
+        box.style.top = (e.clientY - startY) + 'px';
     }
+
+    if (isRotating) {
+        e.preventDefault();
+        const boxRect = box.getBoundingClientRect();
+        const boxCenter = {
+            x: boxRect.left + boxRect.width / 2,
+            y: boxRect.top + boxRect.height / 2
+        };
+        const angle = Math.atan2(e.clientY - boxCenter.y, e.clientX - boxCenter.x);
+        currentRotation = angle - startRotation;
+        box.style.transform = `rotate(${currentRotation}rad)`;
+    }
+
     if (isResizing) {
-        event.preventDefault();
-        const dx = event.clientX - startX;
-        const dy = event.clientY - startY;
+        e.preventDefault();
+        const dx = e.clientX - startX;
+        const dy = e.clientY - startY;
 
         if (currentResizer.classList.contains('bottom-right')) {
             box.style.width = startWidth + dx + 'px';
             box.style.height = startHeight + dy + 'px';
         }
         else if (currentResizer.classList.contains('bottom-left')) {
-            box.style.width = startWidth - dx + 'px';//width tăng
-            box.style.left = startLeft + dx + 'px';//left giảm
-            // box.style.height = startHeight + dy + 'px';
+            box.style.width = startWidth - dx + 'px';
+            box.style.left = startLeft + dx + 'px';
+            box.style.height = startHeight + dy + 'px';
         }
         else if (currentResizer.classList.contains('top-right')) {
             box.style.width = startWidth + dx + 'px';
@@ -78,27 +152,8 @@ document.addEventListener('mousemove', (event) => {
     }
 });
 
-
 document.addEventListener('mouseup', function () {
     isDragging = false;
     isResizing = false;
     isRotating = false;
-});
-
-
-let currentResizer;
-const resizers = document.querySelectorAll('.dot');
-resizers.forEach(resizer => {
-    resizer.addEventListener('mousedown', (event) => {
-        isResizing = true;
-        currentResizer = event.target;
-        startX = event.clientX;
-        startY = event.clientY;
-        startWidth = box.getBoundingClientRect().width;
-        startHeight = box.getBoundingClientRect().height;
-        startLeft = event.offsetLeft;
-        startTop = event.offsetTop;
-        event.stopPropagation();
-        console.log(currentResizer)
-    });
 });
