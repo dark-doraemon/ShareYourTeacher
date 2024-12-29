b2f30d975fe682e9842eec27d9874bd2566983c2
dark-doraemon <tuandinhbi75@gmail.com>
2024-12-29 13:55:29 +0700

    fisnish

--- frontend/ShareYourTeacher/src/app/components/paint/paint.component.ts
+++ frontend/ShareYourTeacher/src/app/components/paint/paint.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ColorPickerModule } from 'ngx-color-picker';
+import { TextboxComponent } from "./textbox/textbox.component";
 @Component({
     selector: 'app-paint',
-    imports: [ColorPickerModule, FormsModule],
+    imports: [ColorPickerModule, FormsModule, TextboxComponent],
     templateUrl: './paint.component.html',
     styleUrl: './paint.component.css'
 })
@@ -11,7 +12,7 @@ export class PaintComponent {
     color: string = '#000000'; // Giá trị mặc định
     penSize: number = 5;
 
-    
+    textboxs : TextboxComponent[] 
     ChangeColor(color: string)
     {
         this.color = color
