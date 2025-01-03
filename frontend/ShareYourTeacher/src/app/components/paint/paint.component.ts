71f43a1aa3dd24d8a1e2d0bb166b9d2d63d05e6b
dark-doraemon <tuandinhbi75@gmail.com>
2025-01-03 07:04:21 +0700

    draw undo clear features

--- frontend/ShareYourTeacher/src/app/components/paint/paint.component.ts
+++ frontend/ShareYourTeacher/src/app/components/paint/paint.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Renderer2, ViewChild } from '@angular/core';
 import { isReactive } from '@angular/core/primitives/signals';
 import { FormsModule } from '@angular/forms';
 import { ColorPickerModule } from 'ngx-color-picker';
@@ -18,52 +18,40 @@ export class PaintComponent implements AfterViewInit {
     context: CanvasRenderingContext2D = null;
     isDrawing = false;
     type: number = 0; 0 //0 là pen, 1 là eraser
-
-
+    isMouseHolding = false;
+    canvasStates = [];
+    canvasStateIndex = -1;
     constructor(private renderer2: Renderer2) {
 
     }
     ngAfterViewInit(): void {
-        this.myCanvasNative = this.myCanvas.nativeElement;
         this.SetUpCanvas();
+
     }
 
     SetUpCanvas() {
-        this.context = this.myCanvasNative.getContext('2d');
-        const cssWidth = this.myCanvasNative.offsetWidth;
-        const cssHeight = this.myCanvasNative.offsetHeight;
-        const scale = window.devicePixelRatio || 1;
-        this.myCanvasNative.width = cssWidth * scale;
-        this.myCanvasNative.height = cssHeight * scale;
-        this.context.scale(scale, scale);
-        // this.context.beginPath();
-        // this.context.moveTo(0, 0);
-        // this.context.lineTo(100, 100);
-        // this.context.stroke();
-        this.context.fillStyle = "#eaeaea";
-        this.context.lineCap = 'round';
+        this.myCanvasNative = this.myCanvas.nativeElement;
+        this.myCanvasNative.width = this.myCanvasNative.parentElement.clientWidth - 50;
+        this.myCanvasNative.height = this.myCanvasNative.parentElement.clientHeight;
+        // this.myCanvasNative.width = ;
+        this.context = this.myCanvasNative.getContext('2d')
+        this.context.fillStyle = '#eaeaea';
+        this.context.fillRect(0, 0, this.myCanvasNative.width, this.myCanvasNative.height)
         this.context.lineWidth = this.penSize;
         this.context.strokeStyle = this.draw_color;
-        this.context.fillRect(0, 0, this.myCanvasNative.width, this.myCanvasNative.height);
-        this.renderer2.listen(this.myCanvasNative, "mousedown", event => this.StartDrawing(event));
+
+        this.renderer2.listen(this.myCanvasNative, 'mousedown', event => this.StartDrawing(event));
         this.renderer2.listen(this.myCanvasNative, 'mousemove', event => this.Draw(event));
-        this.renderer2.listen(this.myCanvasNative, 'mouseup', event => this.StopDrawing(event));
+        this.renderer2.listen(this.myCanvasNative, 'mouseover', event => this.MoveToCanvas(event))
+
     }
 
     StartDrawing(event: MouseEvent) {
         event.preventDefault();
         this.isDrawing = true;
         this.context.beginPath();
-        // console.log(event.clientX - this.myCanvasNative.offsetLeft, event.clientY - this.myCanvasNative.offsetTop);
-        console.log(event.offsetX, event.offsetY);
-        // console.log(event.clientX, event.clientY);
-        // this.context.moveTo(event.clientX - this.myCanvasNative.offsetLeft, event.clientY - this.myCanvasNative.offsetTop);
         this.context.moveTo(event.offsetX, event.offsetY);
-        //   this.context.moveTo(event.offsetX, event.offsetY);
-    }
-
-    StopDrawing(event) {
-        this.isDrawing = false;
+        console.log(event.offsetX, event.offsetY);
     }
 
     Draw(event: MouseEvent) {
@@ -71,12 +59,82 @@ export class PaintComponent implements AfterViewInit {
         if (!this.isDrawing) {
             return;
         }
-        // console.log(event.clientX - this.myCanvasNative.offsetLeft, event.clientY - this.myCanvasNative.offsetTop);
-        // this.context.lineTo(event.clientX - this.myCanvasNative.offsetLeft, event.clientY - this.myCanvasNative.offsetTop);
-        console.log(event.offsetX, event.offsetY);
+        this.context.strokeStyle = this.draw_color;
+        this.context.lineWidth = this.penSize;
         this.context.lineTo(event.offsetX, event.offsetY);
+        this.context.lineCap = "round";
+        this.context.lineJoin = "round";
         this.context.stroke();
     }
+
+    StopDrawing(event) {
+        if(!this.isDrawing)
+        {
+            return;
+        }
+        event.preventDefault();
+        if (this.isDrawing) {
+            this.context.stroke();
+            this.context.closePath();
+        }
+        this.canvasStates.push(this.context.getImageData(0, 0, this.myCanvasNative.width, this.myCanvasNative.height));
+        this.canvasStateIndex += 1;
+        this.isDrawing = false
+    }
+
+    @HostListener('window:mousedown', ['$event'])
+    windowMouseDown(event) {
+        if (event.button === 0) {
+            this.isMouseHolding = true;
+        }
+    }
+
+    @HostListener('window:mouseup', ['$event'])
+    windowMouseUp(event) {
+        if (event.button === 0 ) {
+            this.isMouseHolding = false;
+            this.StopDrawing(event);
+        }
+    }
+
+    @HostListener('window:keydown', ['$event'])
+    KeyDownUndo(event: KeyboardEvent) {
+        if ((event.ctrlKey && event.key == 'z') ) {
+            this.HandleUndo();
+        }
+    }
+
+    ClickUndo(event)
+    {
+        this.HandleUndo()
+    }
+
+
+    HandleUndo()
+    {
+        if (this.canvasStateIndex <= 0) {
+            this.ClearCanvas();
+        }
+        else {
+            this.canvasStateIndex -= 1;
+            this.canvasStates.pop();
+            this.context.putImageData(this.canvasStates[this.canvasStateIndex], 0, 0);
+        }
+    }
+    ClearCanvas() {
+        this.context.fillStyle = '#eaeaea';
+        this.context.clearRect(0, 0, this.myCanvasNative.width, this.myCanvasNative.height);
+        this.context.fillRect(0, 0, this.myCanvasNative.width, this.myCanvasNative.height);
+        this.canvasStates = [];
+        this.canvasStateIndex = -1;
+    }
+
+    MoveToCanvas(event: MouseEvent) {
+        if (this.isMouseHolding) {
+            this.StartDrawing(event);
+        }
+    }
+
 
     ChangeColor(color) {
 
@@ -93,9 +151,9 @@ export class PaintComponent implements AfterViewInit {
         this.myCanvas.nativeElement.style.cursor = `url(https://img.icons8.com/?size=${this.cursorSize}&id=22231&format=png&color=000000) 0 ${this.cursorSize}, auto`
     }
 
-
     ChangePenSize($event: Event) {
-        console.log(($event.target as HTMLInputElement).value);
+        // console.log(($event.target as HTMLInputElement).value);
+        this.penSize = parseInt(($event.target as HTMLInputElement).value, 10);
         if (this.penSize > 5) {
             this.cursorSize = (this.penSize * 20) / 5;
             if (this.cursorSize >= 120) {
