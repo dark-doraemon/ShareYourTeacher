8a7ab62a21d2b442c54e82404db2ee4853c28131
dark-doraemon <tuandinhbi75@gmail.com>
2025-01-08 22:00:59 +0700

    portfolio 1

--- frontend/ShareYourTeacher/src/app/pages/portfolio/portfolio.component.ts
+++ frontend/ShareYourTeacher/src/app/pages/portfolio/portfolio.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-portfolio',
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+    selector: 'app-portfolio',
+    imports: [],
+    templateUrl: './portfolio.component.html',
+    styleUrl: './portfolio.component.css',
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements AfterViewInit {
+
+
+    constructor(private router: Router) {
+
+    }
+
+    ngAfterViewInit(): void {
+    }
+
+
+    scrollToSection(el: HTMLElement) {
+        el.scrollIntoView({ behavior: "smooth" });
+    }
 
 }
