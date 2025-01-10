9343b3f4dadb46b1198d84ac7bcbac0b5c581bdc
dark-doraemon <tuandinhbi75@gmail.com>
2025-01-10 10:31:30 +0700

    finish portfolio

--- frontend/ShareYourTeacher/src/app/pages/portfolio/portfolio.component.ts
+++ frontend/ShareYourTeacher/src/app/pages/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
-import { AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Inject, Renderer2, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,17 +10,39 @@ import { Router } from '@angular/router';
 })
 export class PortfolioComponent implements AfterViewInit {
 
+    @ViewChild('menuicon') menuIcon : ElementRef;
+    @ViewChild('navLinks') navLinks : ElementRef;
 
-    constructor(private router: Router) {
+    constructor(private router: Router,private renderer2 : Renderer2,@Inject(DOCUMENT) private document: Document) {
 
     }
 
     ngAfterViewInit(): void {
+        this.toggleMenu();
+        this.checkIsClickingMenu();
     }
 
 
     scrollToSection(el: HTMLElement) {
         el.scrollIntoView({ behavior: "smooth" });
+    }
+
+    toggleMenu(){
+        this.renderer2.listen(this.menuIcon.nativeElement,'click',(event)=>{
+            this.navLinks.nativeElement.classList.add('active');
+        });
+    }
+
+
+    checkIsClickingMenu()
+    {
+        this.renderer2.listen(this.document,'click',(event : MouseEvent) =>{
+            const element = event.target as HTMLElement;
+            if(element.id !== 'menu-icon')
+            {
+                this.navLinks.nativeElement.classList.remove('active');
+            }
+        });
     }
 
 }
